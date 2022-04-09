019c55ca8e468425ff349d3e50d3b25b6d266d9e
psnaz <psnazle@gmail.com>
2022-04-09 16:13:31 +0000

    Add pprint

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -62,6 +63,17 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defines as the sales figure subtracted from the stock:
+    - Positive surplus indicateds waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    pprint(stock)
+
 
 def main():
     """
@@ -70,6 +82,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
