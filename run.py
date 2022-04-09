8457228243d819b89a88b509e6bec4a7b65c6c8d
psnaz <psnazle@gmail.com>
2022-04-09 15:57:32 +0000

    Amend main function and add welcome

--- run.py
+++ run.py
@@ -62,6 +62,14 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
